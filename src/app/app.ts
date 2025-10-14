2f590f5c860f7fa6f5b9d0117e06675c26d8da5b
Kevinmitsi <kevinandresgarciaaguirre@gmail.com>
2025-10-14 12:38:22 -0500

    creados primeros components y agregado bootstraps al proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Mainpage } from './components/mainpage/mainpage';
+import { NavBar } from './components/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Mainpage, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/mainpage/mainpage.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './mainpage.html',
+  styleUrl: './mainpage.css'
+})
+export class Mainpage {
+
+}
